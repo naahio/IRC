9ae40e9ee198228de86be4f1c8cca06145897e59
Mohamed Babela <mbabela@e2r8p5.1337.ma>
2022-08-15 08:19:53 +0100

    main fct updated

--- srcs/server/server.cpp
+++ srcs/server/server.cpp
@@ -6,13 +6,17 @@
 /*   By: mbabela <mbabela@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 11:44:13 by mbabela           #+#    #+#             */
-/*   Updated: 2022/08/14 12:03:06 by mbabela          ###   ########.fr       */
+/*   Updated: 2022/08/14 13:38:14 by mbabela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <sys/socket.h>
 
-
+struct sockaddr_in {
+   sa_family_t    sin_family; /* address family: AF_INET */
+   in_port_t      sin_port;   /* port in network byte order */
+   struct in_addr sin_addr;   /* internet address */
+};
 //** socket : return (-1) in error or file descriptor : used to creat a socket
 /*
     Domain :
@@ -37,12 +41,39 @@ int socket(int domain, int type, int protocol)
     addr    :
         * a struct used to specify the address to assign to the socket, varies by protocol;
         * the use of 'sockaddr_in' bcs we will use an IP for the server.
+    addlen  :
+        * the addr size;
 */
 
 int bind(int socket_fd, const sockaddr *addr, socklen_t addrlen)
 {
 
 }
+
+//** listen : mark socket to accept the connections : ret (-1) or (0)
+/*
+    socket_fd   :
+        * the file descriptor returned by the socket;
+    backlog     :
+        * the max number of connections allowed ;
+*/
+
+int listen(int socket_fd, int backlog)
+{
+
+}
+
+//** accept : take an element from the socket ret (-1) or (0)
+/*
+    the arguments are the same as bind
+*/
+
+int accept(int sock_fd, sockaddr *addr, socklen_t *addrlen)
+{
+
+}
+
+
 
 int main(int argc, char **argv)
 {
