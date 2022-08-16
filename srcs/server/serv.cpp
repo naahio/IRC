89943fab4df76ff2c32166af8e559c0822d3fe76
Mohamed Babela <mbabela@e2r8p5.1337.ma>
2022-08-16 14:07:04 +0100

    new serv

--- srcs/server/serv.cpp
+++ srcs/server/serv.cpp
@@ -6,42 +6,29 @@
 /*   By: mbabela <mbabela@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 10:53:11 by mbabela           #+#    #+#             */
-/*   Updated: 2022/08/16 11:49:37 by mbabela          ###   ########.fr       */
+/*   Updated: 2022/08/16 13:59:40 by mbabela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/ioctl.h>
-# include <sys/poll.h>
-# include <sys/socket.h>
-# include <sys/time.h>
-# include <netinet/in.h>
-# include <errno.h>
-# include <iostream>
-
-# define SERVER_PORT 9999
-
-# define TRUE           1
-# define FALSE          0
+# include "serv.hpp"
 
 int main(int argc, char **argv)
 {
-    int len              = 1;
-    int rc               = 1;
-    int on               = 1;
-    int socket_fd        = -1;
-    int new_sd           = -1;
+    int len              = TRUE;
+    int rc               = TRUE;
+    int on               = TRUE;
+    int socket_fd        = F_INIT;
+    int new_sd           = F_INIT;
     int desc_ready       = FALSE;
     int end_server       = FALSE;
     int compress_array   = FALSE;
     int close_conn;
-    char buffer[100];
+    char buffer[BUFF_SIZE];
     struct sockaddr_in6 addr;
     int timeout;
-    struct pollfd       fds[200];
-    int     nfds = 1;
-    int     current_size = 0;
+    struct pollfd       fds[POLL_SIZE];
+    int     nfds = TRUE;
+    int     current_size = FALSE;
     int     i;
     int     j;
 
@@ -50,11 +37,54 @@ int main(int argc, char **argv)
     socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
     if (socket_fd < 0)
     {
-        std::cout << "FAILED to create socket, errno " << errno << std::endl;
+        std::cout << "FAILED to create socket, errno : " << errno << std::endl;
         exit (EXIT_FAILURE);
     }
     
-    // 
+    // make the socket fd reuseable : 
 
+    rc = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+    if (rc < 0)
+    {
+        std::cout << "FAILED at set_sock_opt, errno : " << errno << std::endl;
+        close (socket_fd);
+        exit (EXIT_FAILURE);
+    }
     
+    // hadle the part of makingthe spocket nonblocking :
+    
+    rc = ioctl(socket_fd, FIONBIO, (char *)&on);
+    if (rc < 0)
+    {
+        std::cout << "FAILED at making socket nonblocking, errno : " <<  errno << std::endl;
+        close (socket_fd);
+        exit (EXIT_FAILURE);
+    }
+    
+    // assign a PORT and an IP to the socket : 
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin6_family = AF_INET6;
+    memcpy(&addr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
+    addr.sin6_port = htons(SERVER_PORT);
+    rc = bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (rc < 0)
+    {
+        std::cout << "FAILED at binding the socket, errno : " <<  errno << std::endl;
+        close (socket_fd);
+        exit (EXIT_FAILURE);
+    }
+
+    // set the socket to listen : 
+    
+    rc = listen(socket_fd, MAX_CONN);
+    if (rc < 0)
+    {
+        std::cout << "FAILED at listing, errno : " << errno << std::endl;
+        close (socket_fd);
+        exit (EXIT_FAILURE);
+    }
+
+    //
+
 }
---
+++ srcs/server/serv.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   serv.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbabela <mbabela@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/16 13:13:06 by mbabela           #+#    #+#             */
+/*   Updated: 2022/08/16 13:55:19 by mbabela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERV_HPP
+# define SERV_HPP
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/ioctl.h>
+# include <sys/poll.h>
+# include <sys/socket.h>
+# include <sys/time.h>
+# include <netinet/in.h>
+# include <errno.h>
+# include <iostream>
+# include <unistd.h>
+
+# define SERVER_PORT 9999
+
+# define TRUE           1
+# define FALSE          0
+# define F_INIT         -1
+# define BUFF_SIZE      100
+# define POLL_SIZE      200
+# define MAX_CONN       20
+
+
+#endif
