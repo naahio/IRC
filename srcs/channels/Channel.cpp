0973d3e727ad77553f0480bb4b80d622c034879b
Hamza El Makhroubi <hamzaelmakhroubi@gmail.com>
2022-08-30 15:24:00 +0100

    feat: added topic to channels and removed operator from constructor

--- srcs/channels/Channel.cpp
+++ srcs/channels/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 14:19:45 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/08/29 09:53:51 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/08/30 15:04:27 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@
 
 /*************************[ Constructors/Destructors ]*************************/
 
-Channel::Channel(std::string _name, User & _operator) {
+Channel::Channel(std::string _name) {
 	this->name = _name;
-	this->op = &_operator;
+	this->topic = "";
+	this->op = NULL;
 }
 
 /******************************[ Getters/Setters ]*****************************/
 
 std::string const &	Channel::getName(void) const {
 	return (this->name);
+}
+
+std::string const &	Channel::getTopic(void) const {
+	return (this->topic);
 }
 
 User *	Channel::getOperator(void) const {
@@ -37,6 +42,10 @@ void	Channel::setName(std::string _name) {
 	this->name = _name;
 }
 
+void	Channel::setTopic(std::string _topic) {
+	this->topic = _topic;
+}
+
 void	Channel::setOperator(User *	_operator) {
 	this->op = _operator;
 }
@@ -44,6 +53,9 @@ void	Channel::setOperator(User *	_operator) {
 /*****************************[ Member Functions ]*****************************/
 
 void	Channel::addMember(User * member, std::string username) {
+	if (this->members.empty()) {
+		this->op = member;
+	}
 	this->members.insert(std::pair<std::string, User *>(username, member));
 }
 
@@ -52,6 +64,9 @@ void	Channel::removeMember(std::string username) {
 
 	it = this->members.find(username);
 	if (it != this->members.end()) {
+		if (it->second == this->op) {
+			this->op = NULL;
+		}
 		this->members.erase(it);
 	}
 }
